d6dbe5848ad7ebb6f940274690a376643a4f553d
Daniel3147 <daniel.gary@free.fr>
2026-05-29 18:14:19 +0200

    Delete core directory

--- core/php/hyundaikia.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Fichier d'inclusion principal du plugin hyundaikia
- * Chargé automatiquement par Jeedom pour inclure la classe principale */
-require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
